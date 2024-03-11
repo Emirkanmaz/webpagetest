3a0aaab62cec33f9bf75ce52485e17b2211cde31
Emirkanmaz <99258081+Emirkanmaz@users.noreply.github.com>
2024-03-11 21:58:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,16 +5,15 @@ let imageData;
 async function run(event) {
   event.preventDefault();
 
-  const linkedin = document.getElementById('linkedin').value;
-  const dream = document.getElementById('dream').value;
-  const tags = document.getElementById('tags').value;
+  const link = document.getElementById('link').value;
+  const prompt = document.getElementById('prompt').value;
 
   document.getElementById('runButton').style.display = 'none';
   document.getElementById('loadingIndicator').style.display = 'block';
 
   try {
     const app = await client("https://reach-vb-qr-code-ai-art-generator.hf.space/");
-    const result = await app.predict(0, [linkedin, dream, tags, 7.5, 1.1, 0.9, -1, null, null, true, "DPM++ Karras SDE"]);
+    const result = await app.predict(0, [link, prompt, "ugly, disfigured, low quality, blurry, nsfw", 8.5, 1.3, 1, -1, null, null, true, "DPM++ Karras SDE"]);
 
     imageData = result?.data;
 
@@ -36,20 +35,23 @@ async function run(event) {
 function displayImage(base64Data) {
   const imgElement = document.createElement("img");
   imgElement.src = `${base64Data}`;
+  imgElement.style.maxWidth = '100%'; // Ensure the image is responsive
   document.getElementById("imageContainer").innerHTML = "";
   document.getElementById("imageContainer").appendChild(imgElement);
 }
 
 function createDownloadButton() {
-  // "Resmi İndir" butonunu yarat
   const downloadButton = document.createElement("button");
   downloadButton.type = "button";
   downloadButton.id = "downloadButton";
-  downloadButton.style.display = "block";
   downloadButton.textContent = "Resmi İndir";
   downloadButton.onclick = downloadImage;
+  downloadButton.style.display = 'block';
 
-  // "Resmi İndir" butonunu sayfaya ekle
+  // Center the button horizontally
+  downloadButton.style.margin = '0 auto';
+  downloadButton.style.display = 'block'; // Ensure the button is visible
+
   document.body.appendChild(downloadButton);
 }
 
@@ -60,7 +62,7 @@ function downloadImage() {
   a.download = "indirilecek_resim.png";
   document.body.appendChild(a);
   a.click();
-  document.body.removeChild(a); // "Resmi İndir" butonundan bağlantıyı kaldır
+  document.body.removeChild(a);
 }
 
 document.getElementById('inputForm').addEventListener('submit', run);
